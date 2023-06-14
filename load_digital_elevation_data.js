d48389e4e3e3ee1270410fae51db97442f3c15a4
Imildo Sitoe <imildositoe@gmail.com>
2023-06-14 10:14:56 +0200

    Exercises of Digital elevation data from an SRTM google file

---
+++ load_digital_elevation_data.js
@@ -0,0 +1,74 @@
+//// LOAD AND VISUALIZE DATA ////
+
+
+// Load SRTM Digital Elevation Data Version 4 from search bar.
+// Rename it to srtm.
+// Print it.
+
+
+
+// Display the image.
+
+
+
+// Add visualization parameters.
+// Available parameters can be found in the ee.data.getMapId() function documentation.
+
+
+
+// Load feature collection of Montes Mazambulo
+
+
+
+// Print it.
+
+
+
+// Display the feature collection and color it in red.
+
+
+
+// Load Landsat 8 Annual Greenest-Pixel TOA Reflectance Composite from search bar.
+// Rename it to greenestPixel.
+// Print it.
+
+
+
+// Load the 2017 composite and name it green2017.
+
+
+
+// You can also select the image by applying a filter by date function, in two ways:
+
+//green2017 = greenest.filterDate('2017');
+//green2017 = greenest.filter(ee.Filter.date('2017'));
+// Display it in real colors.
+
+
+
+// Display it in false colors to highlight vegetation.
+
+
+
+// Calculate NDVI doing band math.
+
+
+
+// Print NDVI and display it.
+
+
+
+// Calculate NDVI using normalizedDifference function.
+
+
+
+// Load ASTER GDEM tiles of the Montes Mazambulo from own assets.
+
+
+
+// Put the tiles in a collection.
+
+
+
+// Print the collection.
+
